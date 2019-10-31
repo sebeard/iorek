b6fa02a0bd2e29b53490d188cb08ddbdb5eee705
Stuart Beard <sebeard@me.com>
2019-10-31 20:12:18 +0000

    Applying this commit will add a description to the message class for swagger.

--- iorek-api/src/main/java/com/stuartbeard/iorek/api/model/Message.java
+++ iorek-api/src/main/java/com/stuartbeard/iorek/api/model/Message.java
@@ -1,5 +1,6 @@
 package com.stuartbeard.iorek.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Message {
 
+    @ApiModelProperty(value = "A default message that describes the error or message", readOnly = true)
     private String message;
 }
