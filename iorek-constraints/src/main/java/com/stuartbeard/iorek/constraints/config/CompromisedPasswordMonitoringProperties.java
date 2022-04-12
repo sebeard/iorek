3357eacbb8d7ad8f1a8945ba41f75253a3e39d9c
Stuart Beard <sebeard@me.com>
2022-04-12 17:02:48 +0100

    fix: Add Javadoc for CompromisedPasswordMonitoringProperties

--- iorek-constraints/src/main/java/com/stuartbeard/iorek/constraints/config/CompromisedPasswordMonitoringProperties.java
+++ iorek-constraints/src/main/java/com/stuartbeard/iorek/constraints/config/CompromisedPasswordMonitoringProperties.java
@@ -2,6 +2,13 @@ package com.stuartbeard.iorek.constraints.config;
 
 import lombok.Data;
 
+/**
+ * Simple POJO containing configuration properties for monitoring.
+ *
+ * @author Stuart Beard
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @Data
 public class CompromisedPasswordMonitoringProperties {
 
